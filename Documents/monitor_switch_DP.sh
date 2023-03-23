e295943bfff97a616f3d94f53a071ac3e52aa42a
Marek Tomczewski <marek.tomczewski@gmail.com>
2023-03-23 15:49:36 +0100

    Script to change wrong monitor output

--- Documents/monitor_switch_DP.sh
+++ Documents/monitor_switch_DP.sh
@@ -4,16 +4,16 @@
 # Script name   : monitor_switch_DP.sh
 # Description	:                                                                                 
 # Created date  : 26.02.2023
-# Modified date : 26.02.2023
+# Modified date : 23.03.2023
 # Author       	: Marek Tomczewski                                                
 # Email         : marek.tomczewski@gmail.com                                           
 #######################################################################
 
 sudo modprobe i2c-dev
-# Display 3 = middle screen
+# Display 2 = left screen
 # 60 = input
 # 0x0f = DP
 # 0x11 = HDMI
 
-sudo ddcutil -d 3 setvcp 60 0x0f
+sudo ddcutil -d 2 setvcp 60 0x11
 
